f1ece6aa39dc23c26e41930eea834d571cc70b2e
cjling <cjling@aliyun.com>
2015-02-10 12:15:43 +0800

    初始提交

---
+++ build_cjling_directory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+cjling_proj='/home/cjling/001_proj'
+cjling_package='/home/cjling/002_package'
+cjling_repo='/home/cjling/003_repo'
+cjling_vm='/home/cjling/004_vm'
+cjling_mnt='/home/cjling/005_mnt'
+cjling_xxxx='/home/cjling/100_xxxx'
+
+sudo mkdir -p {$cjling_package,$cjling_proj,$cjling_xxxx,$cjling_mnt,$cjling_repo,$cjling_vm}
