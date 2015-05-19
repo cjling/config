6d41bd04392abd94f89cdf0460ddddbd769c1d07
cjling <cjling@aliyun.com>
2015-05-19 10:36:48 +0800

    更正挂载脚本

--- mount_git_server.sh
+++ mount_git_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#git directionay
+git_server='/home/cjling/006_git_server'
+
 #cjling
 #proj_name='k-vim'
 #proj_name='leetcode'
@@ -13,9 +16,9 @@ proj_name='linux_setting'
 #proj_name='ZXCCP-VPlat_CVM-trunk-source.repo-src-openstack-cinder'
 #proj_name='ZXCCP-VPlat_CVM-trunk-source.repo-src-openstack-neutron'
 
-pc_ip='10.74.30.156'
+pc_ip='10.74.30.154'
 path='clone_from_git'
 #path='clone_from_svn'
 
-mkdir $proj_name
-mount -t cifs -o username=10132915,domain=zte.intra //$pc_ip/proj/001_git/$path/$proj_name /home/cjling/006_git_server/$proj_name
+mkdir $git_server/$proj_name
+mount -t cifs -o username=10132915,domain=zte.intra //$pc_ip/proj/001_git/$path/$proj_name $git_server/$proj_name
