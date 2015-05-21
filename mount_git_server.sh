3a75fea4f0432c3776827ab39d21e7737c5c91ab
cjling <cjling@aliyun.com>
2015-05-21 12:12:31 +0800

    改进为自动化脚本

--- mount_git_server.sh
+++ mount_git_server.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-#git directionay
-git_server='/home/cjling/006_git_server'
+#server ip
+pc_ip='10.74.30.154'
 
 #server password
 password='916sa((('
 
-#cjling
-proj_name='k-vim'
-#proj_name='leetcode'
-#proj_name='linux_package'
-#proj_name='linux_setting'
-#proj_name='oh-my-zsh'
+#local git directionay
+git_server='/home/cjling/006_git_server'
 
-#proj
-#proj_name='ZXCCP-VPlat_CVM-trunk-source.repo-src-openstack-nova'
-#proj_name='ZXCCP-VPlat_CVM-trunk-source.repo-src-python-novaclient'
-#proj_name='ZXCCP-VPlat_CVM-trunk-source.repo-src-openstack-cinder'
-#proj_name='ZXCCP-VPlat_CVM-trunk-source.repo-src-openstack-neutron'
+#proj_names
+proj_svn_names=(ZXCCP-VPlat_CVM-trunk-source.repo-src-openstack-nova \
+                ZXCCP-VPlat_CVM-trunk-source.repo-src-python-novaclient \
+                ZXCCP-VPlat_CVM-trunk-source.repo-src-openstack-cinder \
+                ZXCCP-VPlat_CVM-trunk-source.repo-src-openstack-neutron)
+proj_git_names=(k-vim leetcode linux_package linux_setting oh-my-zsh)
 
-pc_ip='10.74.30.154'
-path='clone_from_git'
-#path='clone_from_svn'
+#paths
+path_git='clone_from_git'
+path_svn='clone_from_svn'
 
-mkdir $git_server/$proj_name
-mount -t cifs -o username=10132915,domain=zte.intra,password=$password //$pc_ip/proj/001_git/$path/$proj_name $git_server/$proj_name
+for proj_name in ${proj_git_names[@]}; do
+    mkdir $git_server/$proj_name
+    #umount $git_server/$proj_name
+    #rm $git_server/$proj_name -rf
+    mount -t cifs -o username=10132915,domain=zte.intra,password=$password //$pc_ip/proj/001_git/$path_git/$proj_name $git_server/$proj_name
+done
+
+for proj_name in ${proj_svn_names[@]}; do
+    mkdir $git_server/$proj_name
+    #umount $git_server/$proj_name
+    #rm $git_server/$proj_name -rf
+    mount -t cifs -o username=10132915,domain=zte.intra,password=$password //$pc_ip/proj/001_git/$path_svn/$proj_name $git_server/$proj_name
+done
