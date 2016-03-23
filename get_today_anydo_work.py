39651e8919d7469b152c37c5c1d8ed1ac9f921ff
cjling <cjling@aliyun.com>
2016-03-23 10:06:13 +0800

    修正日志保存

--- get_today_anydo_work.py
+++ get_today_anydo_work.py
@@ -4,7 +4,6 @@
 from anydo_api.client import Client
 from anydo_api.task import Task
 from datetime import datetime
-import sys
 
 
 def get_today_title():
@@ -44,25 +43,6 @@ def save_log(user, tasks):
     print "save log sucessfully!"
 
 
-def print_task(task):
-    if task['status'] == "CHECKED":
-        return
-
-    print "task title:" + task['title']
-
-    if len(task.notes()) != 0:
-        print "task note:"
-        for note in task.notes():
-            print note
-
-    subtask_list = [subtask for subtask in task['subTasks'] if subtask['status'] != "CHECKED"]
-    if len(subtask_list) != 0:
-        print "subtasks:"
-        for subtask in subtask_list:
-            print subtask['title']
-    print "\n\n"
-
-
 def is_doing(due_date):
     now_date = datetime.today()
     if due_date.year < now_date.year:
@@ -81,17 +61,14 @@ def is_doing(due_date):
         return False
 
 
-if __name__ == '__main__':
+def start():
     user = Client(email='cjling@aliyun.com', password='916sa...').get_user()
-
     cate = [cate for cate in user.categories() if cate['name'] == u"工作"]
 
     if len(cate) != 1:
-        sys.exit(0)
+        return
 
     works = cate[0].tasks()
-    now_date = datetime.today()
-    # print "++++++++++++++++++++++++++++++++++++++++++++" + str(now_date.year) + "/" + str(now_date.month) + "/" + str(now_date.day) + "++++++++++++++++++++++++++++++++++++++++++++++++++"
     tasks = []
     for work in works:
         if work['dueDate'] is None:
@@ -99,6 +76,9 @@ if __name__ == '__main__':
         due_date = datetime.fromtimestamp(work['dueDate'] / 1000)
         if is_doing(due_date):
             tasks.append(work)
-            # print_task(work)
 
     save_log(user, tasks)
+
+
+if __name__ == '__main__':
+    start()
