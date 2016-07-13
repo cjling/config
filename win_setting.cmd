464f4d4051112b5d80db25c8173cde78c329d8a9
cjling <cjling@aliyun.com>
2016-07-13 17:06:31 +0800

    提交win配置脚本

---
+++ win_setting.cmd
@@ -0,0 +1,7 @@
+mklink /D C:\Users\Administrator\AppData\Local\Atlassian     D:\ProgramFiles\backup\AppData\Local\Atlassian
+mklink /D C:\Users\Administrator\AppData\Local\Google        D:\ProgramFiles\backup\AppData\Local\Google
+mklink /D C:\Users\Administrator\AppData\Roaming\FileZilla   D:\ProgramFiles\backup\AppData\Roaming\FileZilla
+mklink /D C:\Users\Administrator\AppData\Roaming\SciTools    D:\ProgramFiles\backup\AppData\Roaming\SciTools
+mklink /D C:\Users\Administrator\AppData\Roaming\XMind       D:\ProgramFiles\backup\AppData\Roaming\XMind
+mklink /D C:\Users\Administrator\AppData\Roaming\IM          D:\ProgramFiles\backup\AppData\Roaming\IM
+mklink    C:\Users\Administrator\Documents\AutoHotKey.ahk    E:\proj\git\clone_from_git\xnix_config\AutoHotKey.ahk
