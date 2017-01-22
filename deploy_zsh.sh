3bf096c4e76e710cb1de28a449e5e95d03ecaa6c
cjling <cjling@aliyun.com>
2017-01-22 13:38:55 +0800

    使用yum进行安装

--- deploy_zsh.sh
+++ deploy_zsh.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-git clone root@10.74.120.150:/home/cjling/proj/zsh
-git clone root@10.74.120.150:/home/cjling/proj/autojump
 git clone root@10.74.120.150:/home/cjling/proj/oh-my-zsh
-
-cd /home/cjling/proj/zsh
-./Util/preconfig
-./configure
-make
-make install
-
-cd /home/cjling/proj/autojump
-./install.py
-
-cd /home/cjling/proj/oh-my-zsh
-sh -x link.sh
+yum install zsh
+yum install autojump-zsh
+sh -x ./oh-my-zsh/link.sh
